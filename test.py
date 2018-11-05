535d622b4b3098c39014cb5db6c37fcfcff98716
Vineet Reddy <hotshot2797@gmail.com>
2018-11-05 14:39:51 +0530

    Dataset imported

--- test.py
+++ test.py
@@ -1,7 +1,9 @@
-print("<------ CS303 Machine Learning ------>")
-print("<------ Final Project ------>")
+print("<------ CS303 MACHINE LEARNING ------>")
+print("<------ FINAL PROJECT ------>")
 print("<------ NBA Predictor ------>")
-print("<------ Collaborators ------>")
+print("<------ COLLABORATORS ------>")
 print("<------ Mihika Johorey ------>")
 print("<------ Pratyush Ranjan Tiwari ------>")
 print("<------ Sai Vineet Reddy Thatiparthi ------>")
+print("_   ______  ___       ____  ____  __________  ____________________  ____ 
+   / | / / ")
