dbd7a5428720a93291ea38c7e490ef6a63ba7c80
Vineet Reddy <hotshot2797@gmail.com>
2018-11-05 22:20:12 +0530

    Sort of works

--- main.py
+++ main.py
@@ -6,49 +6,32 @@ import pandas as pd
 
 
 
-#LOADING SHIT INTO LIST
-# with open('NBA_Rankings - 2016-17.csv', 'r') as f:
-#     reader = csv.reader(f)
-#     allStuff = list(reader)
-# print(allStuff)
-# offensivenumberRating = []
-# defensiveRating = []
-# rating = []
-# for i in range(1,31):
-#     offensivenumberRating.append(allStuff[i][10])
-#     defensiveRating.append(allStuff[i][11])
-#     rating.append(float(allStuff[i][12]))
-# print(rating)
-# offensivenumberRating = np.asarray(offensivenumberRating)
-# defensiveRating = np.asarray(defensiveRating)
-# rating = np.asarray(rating)
-# print(rating)
-# print(offensivenumberRating.shape)
+#LOADING DATA USING PANDAS
+rating = pd.read_csv('NBA_Rankings - 2017-18.csv',usecols=['Rating']).values/	110.25
+offensiveRating = pd.read_csv('NBA_Rankings - 2017-18.csv',usecols=['Offensive Rating','Defensive Rating']).values/110.25
 
 
+print(offensiveRating)
 
-#SAME LOADING USING PANDAS
-rating = pd.read_csv('NBA_Rankings - 2016-17.csv',usecols=['Rating']).values
-offensiveRating = pd.read_csv('NBA_Rankings - 2016-17.csv',usecols=['Offensive Rating','Defensive Rating']).values/110
-defensiveRating = pd.read_csv('NBA_Rankings - 2016-17.csv',usecols=['Defensive Rating']).values
-#     allStuff = list(reader)
-
-
-
-rating = np.array([i for [i] in rating])
-
-print(rating)
+#NEURAL NETWORK
 model = Sequential()
 
-model.add(Dense(20, activation='relu',input_dim=2))
-model.add(Dense(20, activation='relu'))
+model.add(Dense(64, activation='relu',input_dim=2))
+model.add(Dense(64, activation='relu'))
+model.add(Dense(10, activation='relu'))
 model.add(Dense(1, activation='relu'))
 
 model.compile(loss='mean_squared_error',
-              optimizer='sgd',
-              metrics=['accuracy'])
+              optimizer='adam',
+              metrics=['mape'])
 
 
 
 model.fit(np.asarray(offensiveRating), np.asarray(rating), epochs=500)
+
+#PREDICTION!
+rating_18 = pd.read_csv('NBA_Rankings - 2016-17.csv',usecols=['Rating']).values/110.25
+offensiveRating_18 = pd.read_csv('NBA_Rankings - 2016-17.csv',usecols=['Offensive Rating','Defensive Rating']).values/110.25
+print(model.predict(offensiveRating_18)*110.25)
+model.layers[1].output
 
