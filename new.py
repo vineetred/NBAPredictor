3e4e48b32ac05325afa79ee5295e86afa53d2542
Vineet Reddy <hotshot2797@gmail.com>
2018-11-21 15:20:00 +0530

    Split the dataset into training and testing

--- new.py
+++ new.py
@@ -1,8 +1,9 @@
 import numpy as np
+import keras
 from keras.models import Sequential
 from keras.layers.core import Dense
-from keras.layers import Dropout
-
+from keras.layers import Dropout,Conv2D
+from sklearn.model_selection import train_test_split
 import csv
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -13,29 +14,28 @@ tok = Tokenizer()
 teams = ['ATL','BOS','BRK', 'CHO', 'CHI', 'CLE', 'DAL', 'DEN', 'DET', 'GSW', 'HOU','IND','LAC','LAL','MEM', 'MIA','MIL','MIN','NOP','NYK','OKC','ORL','PHI','PHO','POR','SAC','SAS','TOR','UTA','WAS']
 # tok.fit_on_texts(teams)
 # tok.texts_to_sequences(teams)
-points = pd.read_csv('nba_games_stats.csv',usecols=['TeamPoints','OpponentPoints','WINLOSS']).values
+points = pd.read_csv('nba_games_stats.csv',usecols=['TeamPoints','OpponentPoints','WINLOSS','FieldGoals','Opp.FieldGoals']).values
 teamStats = pd.read_csv('nba_games_stats.csv',usecols=['Team','Home','Opponent']).values
+X_train, X_test, y_train, y_test = train_test_split(teamStats, points, test_size=0.33)
 
 #Making the neural network
 model = Sequential()
+keras.optimizers.Adam(lr=1)
 model.add(Dense(60, activation='relu',input_dim=3))
-model.add(Dropout(0.25))
-model.add(Dense(36, activation='relu'))
-model.add(Dense(3, activation='relu'))
+model.add(Dropout(0.35))
+# model.add(Dense(46, activation='relu'))
+# model.add(Dense(30, activation='relu'))
+# model.add(Dense(20, activation='relu'))
+model.add(Dense(30, activation='relu'))
+
+
+#CNN
+
+model.add(Dense(5, activation='relu'))
 
 model.compile(loss='mean_squared_error',
               optimizer='adam',
-              metrics=['mse'])
+              metrics=['mape'])
 
-model.fit(np.asarray(teamStats), np.asarray(points), epochs=1000)
+model.fit(np.asarray(X_train), np.asarray(y_train), epochs=1000)
 
-hello = np.array([[15,1,1]])
-prediction = model.predict(hello)
-# print(teams[hello[0][0]])
-if(prediction[0][1]>prediction[0][2]):
-  print(teams[hello[0][0]],"wins against",teams[hello[0][2]])
-  print("Score: ",prediction[0][1], "-",prediction[0][2])
-
-else:
-  print(teams[hello[0][2]], " wins against", teams[hello[0][0]])
-  print("Score: ",prediction[0][2], "-",prediction[0][1])
